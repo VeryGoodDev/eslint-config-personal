156c6a6a84330c577158b96a8587f86255081331
VeryGoodDev <very_good_dev@outlook.com>
2022-04-14 23:37:16 -0700

    Exporting preact configs

---
+++ configs/preact-ts.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [`./ts-base.js`, `./react-base.js`],
+  settings: {
+    react: {
+      pragma: `h`,
+      fragment: `Fragment`,
+      version: `17.0`,
+    },
+    formComponents: [{ name: `Form`, formAttribute: `endpoint` }],
+    linkComponents: [{ name: `Link`, linkAttribute: `to` }],
+  },
+}
