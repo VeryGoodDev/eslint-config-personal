c29d6124f1e115a76fd375ae85b6fed9b7e17fab
VeryGoodDev <very_good_dev@outlook.com>
2022-04-14 23:36:50 -0700

    Exporting browser configs

---
+++ configs/browser-js.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [`./eslint-base.js`],
+  env: {
+    browser: true,
+    webextensions: true,
+    worker: true,
+  },
+}
---
+++ configs/browser-ts.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [`./ts-base.js`],
+  env: {
+    browser: true,
+    webextensions: true,
+    worker: true,
+  },
+}
