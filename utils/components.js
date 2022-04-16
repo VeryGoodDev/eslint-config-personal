d3d6cfdafe8fd79c95efb7ed4aa9720a2269be46
VeryGoodDev <very_good_dev@outlook.com>
2022-04-16 00:32:04 -0700

    Create util for list of component names

---
+++ utils/components.js
@@ -0,0 +1,9 @@
+module.exports = {
+  audioComponents: [`Audio`],
+  formComponents: [`Form`],
+  headingComponents: [`Heading`],
+  imageComponents: [`Image`],
+  inputComponents: [`NumberInput`, `PhoneInput`, `TextInput`],
+  linkComponents: [`Link`],
+  videoComponents: [`Video`],
+}
