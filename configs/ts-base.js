6138df8bde128d5c4007622dba3951f3fb6f2ddb
VeryGoodDev <very_good_dev@outlook.com>
2022-05-04 00:22:05 -0700

    Allow voiding promises

--- configs/ts-base.js
+++ configs/ts-base.js
@@ -337,7 +337,7 @@ module.exports = {
     '@typescript-eslint/no-floating-promises': [
       `error`,
       {
-        ignoreVoid: false,
+        ignoreVoid: true,
         ignoreIIFE: false,
       },
     ],
