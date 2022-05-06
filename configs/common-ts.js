dccf0cfd10f3d098fe7725e35af2d7fa46754135
VeryGoodDev <very_good_dev@outlook.com>
2022-05-06 00:34:39 -0700

    Turn off extension requirement in TS config

--- configs/common-ts.js
+++ configs/common-ts.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: [`./ts-base.js`, `./common-base.js`],
   rules: {
+    // Not really needed in most if not all scenarios with TS
+    'import/extensions': `off`,
     // Handled by TS, no need for eslint to pull double duty on it
     'import/named': `off`,
   },
